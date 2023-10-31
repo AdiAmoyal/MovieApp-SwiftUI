16d5d463bba74a6691bba05d53dce23d006511f9
Adi Amoyal <adi.amoyal02@gmail.com>
2023-10-31 22:54:38 +0200

    Initial Commit

---
+++ MovieApp-SwiftUI/MovieApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_SwiftUIApp.swift
+//  MovieApp-SwiftUI
+//
+//  Created by Adi Amoyal on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
